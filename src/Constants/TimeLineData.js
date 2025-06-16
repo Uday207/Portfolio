44b5f682315f618c01e60244e1f7b3dd3976d16a
Uday207 <chinthamanuudaykumar@gmail.com>
2025-06-16 12:30:11 +0530

    edited details

--- src/Constants/TimeLineData.js
+++ src/Constants/TimeLineData.js
@@ -2,7 +2,7 @@ import { FaBriefcase, FaGraduationCap, FaBuilding } from 'react-icons/fa';
 
 const timelineItems = [
   {
-    date: <span className='text-lightText md:text-darkBg dark:text-lightText'> 2023 MAY - Present </span>,
+    date: <span className='text-lightText md:text-darkBg dark:text-lightText'> 2022 Jun - Present </span>,
     position: 'right',
     icon: <FaBriefcase />,
     title: 'Tanama Software Private Limited',
@@ -10,12 +10,12 @@ const timelineItems = [
     description: 'React JS, Redux Toolkit, React-Router, TailWindCSS, GraphQL, NextJS, MySQL'
   },
   {
-    date: <span className='text-lightText md:text-darkBg dark:text-lightText'>2021 APR - 2023 APR</span>,
+    date: <span className='text-lightText md:text-darkBg dark:text-lightText'>2022 Aug - 2023 Aug</span>,
     position: 'left',
-    icon: <FaBriefcase />,
-    title: 'Border Security Force',
-    subTitle: 'GD Constable (Central Govt Job)',
-    description: 'Ensured the security and integrity of the Indian border areas under para military Force',
+    icon: <FaGraduationCap />,
+    title: 'Coding Ninjas',
+    subTitle: 'Full Stack Develoment with Java and Data Structures',
+    description: 'It is an online Course Platform To learn Java, Java Script, Data Structures, Forntend, React and also backend nodejs,MySQL Data Base, Also MongoDB',
   },
   {
     date: <span className='text-lightText md:text-darkBg dark:text-lightText'> 2018 - 2020 </span>,
